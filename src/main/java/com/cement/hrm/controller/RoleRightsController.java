5fa60aa69f1c57fac9008375eeef1dc742864fb1
amit4uindia <amit.p@cementdigital.com>
2023-10-10 11:42:52 +0530

    Role right and holiday api added.

--- src/main/java/com/cement/hrm/controller/RoleRightsController.java
+++ src/main/java/com/cement/hrm/controller/RoleRightsController.java
@@ -3,6 +3,7 @@ package com.cement.hrm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cement.hrm.constant.UrlConstants;
@@ -33,5 +35,10 @@ public class RoleRightsController {
 	@PostMapping(UrlConstants.ADD_EDIT)
 	public String addEditRole(@RequestBody String roleRightsJson) {
 		return roleRightsService.addEditRoleRights(roleRightsJson);
+	}
+	@GetMapping(UrlConstants.GET_BY_ID)
+	@Transactional(readOnly = true)
+	public ResponseEntity<?> getRoleRightsById(@RequestParam int roleId) {
+		return new ResponseEntity<>(roleRightsService.getRoleRightsById(roleId),HttpStatus.OK);
 	}
 }
--- src/main/java/com/cement/hrm/request/HolidayRequest.java
+++ src/main/java/com/cement/hrm/request/HolidayRequest.java
@@ -6,21 +6,20 @@ public class HolidayRequest {
 
 	private Date fromDate;
 	private Date toDate;
+	private Integer employeeId;
+	private Integer holidayId;
+	private Integer status;
 
-	private int status;
+	public HolidayRequest() {
+		super();
+	}
 
-	public HolidayRequest(Date fromDate, Date toDate, int status) {
+	public HolidayRequest(Date fromDate, Date toDate, Integer employeeId, Integer holidayId, Integer status) {
 		super();
 		this.fromDate = fromDate;
 		this.toDate = toDate;
-		this.status = status;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
+		this.employeeId = employeeId;
+		this.holidayId = holidayId;
 		this.status = status;
 	}
 
@@ -38,6 +37,30 @@ public class HolidayRequest {
 
 	public void setToDate(Date toDate) {
 		this.toDate = toDate;
+	}
+
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public Integer getHolidayId() {
+		return holidayId;
+	}
+
+	public void setHolidayId(Integer holidayId) {
+		this.holidayId = holidayId;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
 	}
 
 }
